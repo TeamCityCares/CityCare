e6df3f6f67da0e9da3d571b97875e5568567db83
mugadavinaykumar <MugadaVinay.Kumar@cognizant.com>
2026-03-05 14:30:09 +0530

    commit

---
+++ CityCare/src/main/java/com/cts/CityCare/CityCare/CityCareApplication.java
@@ -0,0 +1,14 @@
+package com.cts.CityCare.CityCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityCareApplication.class, args);
+		System.out.println("CityCareApplication started");
+	}
+
+}
---
+++ CityCare/src/test/java/com/cts/CityCare/CityCare/CityCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.CityCare.CityCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
