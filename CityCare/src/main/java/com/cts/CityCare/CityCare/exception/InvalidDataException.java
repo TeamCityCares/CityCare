6099f2bbdbe2916cc5782fd5497a158d4c987271
Satish-276 <Pakalapati.Satish@cognizant.com>
2026-03-06 16:29:07 +0530

    Added Exceptions Assuming the Situations

---
+++ CityCare/src/main/java/com/cts/CityCare/CityCare/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package com.cts.CityCare.CityCare.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
