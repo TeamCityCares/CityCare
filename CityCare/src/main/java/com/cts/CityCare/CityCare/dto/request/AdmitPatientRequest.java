4dc38842828f01f4f6a3825c12916391de73040a
mugadavinaykumar <MugadaVinay.Kumar@cognizant.com>
2026-03-13 16:01:30 +0530

    updated code

--- CityCare/src/main/java/com/cts/CityCare/CityCare/dto/request/AdmitPatientRequest.java
+++ CityCare/src/main/java/com/cts/CityCare/CityCare/dto/request/AdmitPatientRequest.java
@@ -1,14 +1,14 @@
 package com.cts.CityCare.CityCare.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 
 @Data
 public class AdmitPatientRequest {
 
     @NotNull(message = "Emergency ID is required")
     private Long emergencyId;
-
     private String ward;  // e.g. "ICU", "General Ward", "Cardiology"
     private String notes; // Admission notes
 }
