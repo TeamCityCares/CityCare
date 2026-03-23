7f1b546a42ad6528ac4ecf06155647e9ad4bf4e5
shyamlal.js@cognizant.com <shyamlal.js@cognizant.com>
2026-03-23 09:12:14 +0530

    Added validations

--- CityCare/src/main/java/com/cts/CityCare/CityCare/dto/request/DispatchRequest.java
+++ CityCare/src/main/java/com/cts/CityCare/CityCare/dto/request/DispatchRequest.java
@@ -1,11 +1,15 @@
 package com.cts.CityCare.CityCare.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class DispatchRequest {
 
     @NotNull(message = "Ambulance ID is required")
+    @Positive(message = "ambulance Id must be positive")
+    @Min(value = 1,message = "ambulanceId must start from 1")
     private Long ambulanceId;
 }
